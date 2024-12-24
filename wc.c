acdd7f063d0f38b2942f63cfabb0b59bf25825eb
VictorPochettini <vpochettini@gmail.com>
2024-12-24 17:11:42 -0300

    Update wc.c
    Error Message!

--- wc.c
+++ wc.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <errno.h>
 
 int main( int argc, char* argv[])
 {
-    
+    if (argc == NULL)
+    {
+        perror("Error: No file informed.")
+    }
     return 0;
 }
