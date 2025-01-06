d8a925169fc8d783f7fed822f38b81e3fbaa9d71
VictorPochettini <vpochettini@gmail.com>
2025-01-06 10:46:24 -0300

    Command Reading
    Updated the way a command is read by the program. There are still mistakes, such as not considering the possibility of an operation without the operation marker (-w, -c, etc)

--- wc.c
+++ wc.c
@@ -1,18 +1,67 @@
 #include <stdio.h>
 
-int main( int argc, char* argv[])
+int procuraTraco(char comando[])
 {
-    if (argc == NULL)
-    {
-        fprintf(stderr, "Error: No file informed.\n");
-    }
+	int i = 0;
+	while(comando[i] != '-' || comando[i] != '\0')
+	{
+		i++;
+	}
+	return i;
+}
 
-    FILE* file = fopen(argv, "r");
-    if(file == NULL)
-    {
-       puts("Number of characters, lines, words and bytes equal zero");
-       fclose(file);
-    }
+char verificaCom(int indice, char comando[])
+{
+	char letra = comando[indice+1];
+	return letra;
+}
 
-    return 0;
+char passaCaminho(int indice, char comando[])
+{
+	char caminho[256];
+	int i = 3;
+	do
+	{
+		caminho[i] = comando[indice+i];
+		i++;
+	}while(comando[indice+i] != '\0');
+
+	return caminho[256];
+}
+
+int main()
+{
+
+	char comando[512];
+	
+	printf(">vwc: ");
+	fgets(comando, sizeof(comando), stdin);
+	printf("\n");
+	
+	int indice = procuraTraco(comando[]);
+	
+	if(indice == sizeof(comando)/8)
+	{
+		//Fazer ele executar todas as instruções
+		return 0;
+	}
+
+	char caminho[256] = passaCaminho(indice, comando);
+	
+
+	opcao = verificaCom(indice, comando);
+
+	switch(opcao)
+	{
+		case 'c':
+			//Número de Bytes
+		case 'l':
+			//Número de Linhas
+		case 'w':
+			//Número de Palavras
+		case 'm':
+			//Número de Caracteres
+	}	
+
+	return 0;
 }
