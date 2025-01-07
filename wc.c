7dc2b96833cf03cbb3a2d9492107fd1ae20af5c8
VictorPochettini <vpochettini@gmail.com>
2025-01-07 19:23:29 -0300

    wc.c
    Corrected mistakes regarding some functions. Also added a function to count how many lines a file has.

--- wc.c
+++ wc.c
@@ -1,84 +1,115 @@
 #include <stdio.h>
+#define MAX 10000000
 
 int procuraTraco(char comando[])
 {
-	int i = 0;
-	while(comando[i] != '-' && comando[i] != '\0')
-	{
-		i++;
-	}
-	return i;
+    int i = 0;
+    while (comando[i] != '-' && comando[i] != '\0')
+    {
+        i++;
+        if (comando[i] == 27)
+        {
+            return 27;
+        }
+    }
+    return i;
 }
 
 char verificaCom(int indice, char comando[])
 {
-	char letra = comando[indice+1];
-	return letra;
+    char letra = comando[indice + 1];
+    return letra;
 }
 
-void passaCaminho(int indice, char comando[], char caminho[], int* espaco, int espaco2)
+void passaCaminho(int indice, char comando[], char caminho[], int* espaco)
 {
-	int i = 0;
-	while (comando[indice + i] != '\0' && comando[indice + i] != '\n')
-	{
-		caminho[i] = comando[indice+i];
-		if(caminho[i] == ' ' && *espaco == -1)
-		{
-			*espaco = i;
-		}
-		i++;
+    int i = 2;
+    while (comando[indice + i] != '\0' && comando[indice + i] != '\n')
+    {
+        caminho[i] = comando[indice + i];
+        if (caminho[i] == ' ' && *espaco == -1)
+        {
+            *espaco = i;
+        }
+        i++;
+        if (caminho[i] == ' ' && *espaco != -1)
+        {
+            fprintf(stderr, "Error: too many files.\n");
+            return;
+        }
+    }
+    caminho[i] = '\0';
+}
 
-		if(caminho[i] == ' ' && *espaco != -1)
-		{
-			*espaco2 = i;
-		}
-		i++;
-		if(caminho[i] == ' ' && *espaco2 != -1)
-		{
-			fprintf(stderr, "Error: No file informed.\n");
-		}
-	}
+void montaCaminho(char caminho[], int espaco, char caminho1[], char caminho2[])
+{
+    if (espaco != -1)
+    {
+        caminho1[espaco] = '\0';
+        for (int i = espaco + 1; caminho[i] != '\0'; i++)
+        {
+            caminho2[i - espaco - 1] = caminho[i];
+        }
+        caminho2[espaco] = '\0';
+    }
+}
 
-	caminho[i] = '\0';
+int l(FILE* file)
+{
+    char a[MAX];
+    int acc = 1;
+    
+    while (fgets(a, MAX, file))
+    {
+        acc++;
+    }
+    
+    return acc;
 }
 
 int main()
 {
-
-	char comando[512];
-	char caminho[256];
-	int espaco = -1;
-	int* espaco2 = espaco + 1;
-	
-	printf(">vwc: ");
-	fgets(comando, sizeof(comando), stdin);
-	printf("\n");
-	
-	int indice = procuraTraco(comando);
-	
-	if(indice == sizeof(comando)/8)
-	{
-		//Fazer ele executar todas as instruções
-		return 0;
-	}
-
-	passaCaminho(indice, comando, caminho, espaco, espaco2);
-
-	
-
-	opcao = verificaCom(indice, comando);
-
-	switch(opcao)
-	{
-		case 'c':
-			//Número de Bytes
-		case 'l':
-			//Número de Linhas
-		case 'w':
-			//Número de Palavras
-		case 'm':
-			//Número de Caracteres
-	}	
-
-	return 0;
+    while (1)
+    {
+        char comando[512];
+        char caminho[256];
+        int espaco = -1;
+        int linhas = 1;
+        FILE* file;
+        FILE* file2;
+        char caminho1[256];
+        char caminho2[256];
+        char opcao;
+    
+        printf(">vwc: ");
+        fgets(comando, sizeof(comando), stdin);
+        printf("\n");
+    
+        int indice = procuraTraco(comando);
+    
+        if (indice == sizeof(comando) / 8)
+        {
+            return 0;
+        }
+    
+        passaCaminho(indice, comando, caminho, &espaco);
+        montaCaminho(caminho, espaco, caminho1, caminho2); 
+    
+        opcao = verificaCom(indice, comando);
+    
+        switch (opcao)
+        {
+            case 'c':
+                break;
+            case 'l':
+                linhas = l(file);
+                printf("Linhas: %d\n", linhas);
+                break;
+            case 'w':
+                break;
+            case 'm':
+                break;
+        }
+    }
+    return 0;
 }
