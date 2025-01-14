8c9321ed92b7e0976db472bf6374814217f1e8f7
VictorPochettini <vpochettini@gmail.com>
2025-01-14 19:31:00 -0300

    Took out some bugs

--- wc.c
+++ wc.c
@@ -5,67 +5,62 @@
 
 int procuraTraco(char comando[])
 {
-    int i = 0;
-    while (comando[i] != '-' && comando[i] != '\0')
-    {
-        i++;
-    }
-    return i;
+	int i = 0;
+	while (comando[i] != '-' && comando[i] != '\0')
+	{
+		i++;
+	}
+	return (comando[i] == '-') ? i : -1;
 }
 
 char verificaCom(int indice, char comando[])
 {
-    char letra = comando[indice + 1];
-    return letra;
+	return comando[indice + 1];
 }
 
 void passaCaminho(int indice, char comando[], char caminho[], int* espaco)
 {
-    int i = 3;
-    while (comando[indice + i] != '\0' && comando[indice + i] != '\n')
-    {
-        caminho[i] = comando[indice + i];
-        if (caminho[i] == ' ' && *espaco == -1)
-        {
-            *espaco = i;
-        }
-        i++;
-        if (caminho[i] == ' ' && *espaco != -1)
-        {
-            fprintf(stderr, "Error: too many files.\n");
-            return;
-        }
-    }
-    caminho[i] = '\0';
+	int i = 3;
+	while (comando[indice + i] != '\0' && comando[indice + i] != '\n')
+	{
+		caminho[i] = comando[indice + i];
+		if (caminho[i] == ' ' && *espaco == -1)
+		{
+			*espaco = i;
+		}
+		i++;
+	}
+	caminho[i] = '\0';
 }
 
 void montaCaminho(char caminho[], int espaco, char caminho1[], char caminho2[])
 {
-    memset(caminho1, 0, 256);
-    memset(caminho2, 0, 256);
+	memset(caminho1, 0, 256);
+	memset(caminho2, 0, 256);
 
-    if (espaco != -1)
-    {
-        for (int i = 0; i < espaco; i++)
-        {
-            caminho1[i] = caminho[i];
-        }
-        caminho1[espaco] = '\0';
+	if (espaco != -1)
+	{
+		for (int i = 0; i < espaco; i++)
+		{
+			caminho1[i] = caminho[i];
+		}
+		caminho1[espaco] = '\0';
 
-        int j = 0;
-        for (int i = espaco + 1; caminho[i] != '\0'; i++, j++)
-        {
-            caminho2[j] = caminho[i];
-        }
-        caminho2[j] = '\0';
-    }
-    else
-    {
-        strcpy(caminho1, caminho);
-    }
+		int j = 0;
+		for (int i = espaco + 1; caminho[i] != '\0'; i++, j++)
+		{
+			caminho2[j] = caminho[i];
+		}
+		caminho2[j] = '\0';
+	}
+	else
+	{
+		strcpy(caminho1, caminho);
+	}
 }
 
-int c(FILE* file) {
+int c(FILE* file)
+{
 	int acc = 0;
 	char ch;
 	while ((ch = fgetc(file)) != EOF)
@@ -81,7 +76,7 @@ int l(FILE *file)
 	char ch;
 	while ((ch = fgetc(file)) != EOF)
 	{
-        	if (ch == '\n')
+		if (ch == '\n')
 		{
 			acc++;
 		}
@@ -97,34 +92,38 @@ int w(FILE *file)
 	{
 		if ((ch == ' ' || ch == '\n') && prev != ' ' && prev != '\n')
 		{
-		acc++;
-        	}
-	prev = ch;
+			acc++;
+		}
+		prev = ch;
 	}
 	return acc;
 }
 
-long m(FILE* file, char caminho[])
+long m(FILE* file, const char caminho[])
 {
 	fclose(file);
 	file = fopen(caminho, "rb");
-	if(!file)
+	if (!file)
 	{
-		fprintf(stderr, "Error: file %s does not exist", caminho);
+		fprintf(stderr, "Error: file %s does not exist\n", caminho);
+		return -1;
 	}
 	fseek(file, 0, SEEK_END);
 	long bytes = ftell(file);
+	fclose(file);
 	return bytes;
 }
 
-void todos(file, caminho)
+void todos(FILE* file, const char caminho[])
 {
 	printf("Caracteres: %d\n", c(file));
 	printf("Linhas: %d\n", l(file));
 	printf("Palavras: %d\n", w(file));
-	printf("Bytes: %ld\n", m(file, caminho1));
-	fclose(file);
-	
+	long bytes = m(file, caminho);
+	if (bytes != -1)
+	{
+		printf("Bytes: %ld\n", bytes);
+	}
 }
 
 int main()
@@ -132,94 +131,88 @@ int main()
 	while (1)
 	{
 		char comando[512];
-        	char caminho[256];
-        	int espaco = -1;
-        	FILE* file;
-        	FILE* file2;
-        	char caminho1[256];
-        	char caminho2[256];
-        	char opcao;
-		int tipo = 0;    
+		char caminho[256];
+		int espaco = -1;
+		FILE* file = NULL;
+		FILE* file2 = NULL;
+		char caminho1[256];
+		char caminho2[256];
 
-        	printf(">vwc: ");
-        	fgets(comando, sizeof(comando), stdin);
-        	printf("\n");
-
-		for(int i = 0; i<sizeof(comando); i++)
+		printf(">vwc: ");
+		if (!fgets(comando, sizeof(comando), stdin))
 		{
-			if(comando[i] == 27)
-			goto end;
+			break;
 		}
-    
-        	int indice = 0;
 
-        	passaCaminho(indice, comando, caminho, &espaco);
-        	montaCaminho(caminho, espaco, caminho1, caminho2); 
+		if (strchr(comando, 27))
+		{
+			break;
+		}
+
+		int tracoPos = procuraTraco(comando);
+		if (tracoPos == -1)
+		{
+			// No commands, just file paths; execute all commands
+			sscanf(comando, "%s %s", caminho1, caminho2);
+			file = fopen(caminho1, "r");
+			if (!file)
+			{
+				fprintf(stderr, "Error: file %s does not exist\n", caminho1);
+				continue;
+			}
+			todos(file, caminho1);
+			fclose(file);
+
+			if (strlen(caminho2) > 0)
+			{
+				file2 = fopen(caminho2, "r");
+				if (!file2)
+				{
+					fprintf(stderr, "Error: file %s does not exist\n", caminho2);
+					continue;
+				}
+				todos(file2, caminho2);
+				fclose(file2);
+			}
+			continue;
+		}
+
+		passaCaminho(tracoPos, comando, caminho, &espaco);
+		if (strchr(caminho + espaco + 1, ' ') != NULL)
+		{
+			fprintf(stderr, "Error: too many files.\n");
+			continue;
+		}
+		montaCaminho(caminho, espaco, caminho1, caminho2);
 
 		file = fopen(caminho1, "r");
-		file2 = fopen(caminho2, "r");
-
-		if(!file)
+		if (!file)
 		{
-			fprintf(stderr, "Error: file %s does not exist", caminho1);
-			break;
+			fprintf(stderr, "Error: file %s does not exist\n", caminho1);
+			continue;
 		}
 
-		if(!file2)
+		char opcao = verificaCom(tracoPos, comando);
+		switch (opcao)
 		{
-			tipo = 1;
-		}
-
-		if (indice == sizeof(comando) / 8)
-		{
-			todos(file, caminho1);
-			if(tipo == 0) todos(file2, caminho2);
-			break;
-        	}
-    
-        	opcao = verificaCom(indice, comando);
-    
-        	switch (opcao)
-        	{
 			case 'c':
 				printf("Caracteres: %d\n", c(file));
-				if(tipo == 0)
-				{
-					printf("Caracteres: %d\n", c(file2));
-				}
 				break;
 			case 'l':
-            			printf("Linhas: %d\n", l(file));
-				if(tipo == 0)
-					{
-						printf("Linhas: %d\n", l(file2));
-					}
+				printf("Linhas: %d\n", l(file));
 				break;
 			case 'w':
-            			printf("Palavras: %d\n", w(file));
-				if(tipo == 0)
-				{
-					printf("Palavras: %d\n", w(file2));
-				}
+				printf("Palavras: %d\n", w(file));
 				break;
 			case 'm':
 				printf("Bytes: %ld\n", m(file, caminho1));
-				if(tipo == 0)
-				{
-					printf("Bytes: %ld\n", m(file2, caminho2));
-				}
 				break;
-			case 27:
-				break;
-
 			default:
 				fprintf(stderr, "Error: Invalid command\n");
 				break;
-                	
+		}
 
-        	}
 		fclose(file);
-		if(file2) fclose(file2);
-	
 	}
+	return 0;
 }
